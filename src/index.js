89e74049dddb02a02bc04439bbee5fe096421032
slamm <slamm@thuzio.com>
2018-02-12 16:33:31 -0500

    add morgan for logging

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ const express = require('express')
 const app = express()
 const path = require('path')
 const pug = require('pug')
+const morgan = require('morgan')
 
 app.set('views', path.join(__dirname, 'views'))
 app.use('/static', express.static(path.join(__dirname, 'views')))
 app.engine('html', require('pug').renderFile)
+app.use(morgan('tiny'))
 
 app.get('/test', (req, res) => {
 	res.render('test.html')
